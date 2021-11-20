42eb9ed1b3468f9a99764410ac602744246127fa
Ido Strauss <strauss02@hotmail.co.il>
2021-11-20 14:14:03 +0200

    basic styling

---
+++ muza/src/components/Board.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Board() {
+  return (
+    <div className=" h-3/4 bg-gray-500 w-1/2">
+      <h1> Hello World</h1>
+    </div>
+  )
+}
+
+export default Board
